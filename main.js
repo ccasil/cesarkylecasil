aa201f7f502d8ae55b1bcc20a3ea33ae304b554d
ccasil <cesarkylecasil@gmail.com>
2018-08-15 18:21:30 -0700

    fade in time and point

--- main.js
+++ main.js
@@ -22,9 +22,9 @@
                     var bottom_of_window = $(window).scrollTop() + $(window).height();
 
                     /* If the object is completely visible in the window, fade it in */
-                    if (bottom_of_window > bottom_of_object) {
+                    if (bottom_of_window > bottom_of_object - 75) {
 
-                        $(this).animate({ 'opacity': '1' }, 800);
+                        $(this).animate({ 'opacity': '1' }, 400);
 
                     }
 
