246ce7c29d673880814bf390d7410a6875b1fd8c
ccasil <cesarkylecasil@gmail.com>
2018-08-21 23:20:11 -0700

    removed comments/unwanted whitespace

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 (function ($) {
     $(document).ready(function () {
-
         var scroll = $(window).scrollTop();
         scroll = scroll + $(window).height() - 50;
-        
-        // $('html, body').animate({
-        //     scrollTop: scroll
-        // }, 1500);
 
         $(function () {
             $(window).scroll(function () {
@@ -15,29 +10,20 @@
                 } else {
                     $('.navbar').removeClass('solid');
                 }
-                /* Check the location of each desired element */
+                
                 $('.hideme').each(function (i) {
-
                     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
                     var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-                    /* If the object is completely visible in the window, fade it in */
                     if (bottom_of_window > bottom_of_object - 75) {
-
                         $(this).animate({ 'opacity': '1' }, 400);
-
                     }
-
                 }); 
             });
         });
 
-        // Add smooth scrolling to all links
         $("a").on('click', function (event) {
             if (this.hash !== "") {
-                // Prevent default anchor click behavior
                 event.preventDefault();
-                // Store hash
                 var hash = this.hash;
                 $('html, body').animate({
                     scrollTop: $(hash).offset().top
