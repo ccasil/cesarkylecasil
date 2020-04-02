888814c0219c8903a36b05128049f28ffadfee61
ccasil <cesarkylecasil@gmail.com>
2020-04-02 15:35:48 -0700

    styling

--- main.js
+++ main.js
@@ -21,7 +21,7 @@
                 var hash = this.hash;
                 $('html, body').animate({
                     scrollTop: $(hash).offset().top
-                }, 600, function () {
+                }, 400, function () {
                     window.location.hash = hash;
                 });
             }
