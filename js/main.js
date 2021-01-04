28fa1c60a68a987a49386f7482ad9d80b3012c61
ccasil <cesarkylecasil@gmail.com>
2021-01-03 19:14:50 -0800

    update

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
                 $('.hideme').each(function (i) {
                     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
                     var bottom_of_window = $(window).scrollTop() + $(window).height();
-                    if (bottom_of_window > bottom_of_object - 75) {
+                    if (bottom_of_window > bottom_of_object - 100) {
                         $(this).animate({
                             'opacity': '1'
                         }, 300);
