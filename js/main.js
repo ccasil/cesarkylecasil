c8c3d5511aad2846d8bb01c82fd88760ae586480
Cesar Kyle Casil <ccasil@users.noreply.github.com>
2024-07-22 11:50:23 -0700

    update bootstrap

--- js/main.js
+++ js/main.js
@@ -1,39 +1,40 @@
-$(document).ready(() => {
-    $(window).scroll(() => {
-        $('.hideme').each((i, element) => {
-            const bottom_of_object = $(element).offset().top + $(element).outerHeight();
-            const bottom_of_window = $(window).scrollTop() + $(window).height();
-            if (bottom_of_window > bottom_of_object - 100) {
-                $(element).animate({
-                    'opacity': '1'
-                }, 300);
-            }
-        });
-    });
+// $(document).ready(() => {
+//     $(window).scroll(() => {
+//         $('.hideme').each((i, element) => {
+//             const bottom_of_object = $(element).offset().top + $(element).outerHeight();
+//             const bottom_of_window = $(window).scrollTop() + $(window).height();
+//             if (bottom_of_window > bottom_of_object - 100) {
+//                 $(element).animate({
+//                     'opacity': '1'
+//                 }, 300);
+//             }
+//         });
+//     });
 
-    $("a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            const hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 400, () => {
-                window.location.hash = hash;
-            });
-        }
-    });
+//     $("a").on('click', function (event) {
+//         if (this.hash !== "") {
+//             event.preventDefault();
+//             const hash = this.hash;
+//             $('html, body').animate({
+//                 scrollTop: $(hash).offset().top
+//             }, 400, () => {
+//                 window.location.hash = hash;
+//             });
+//         }
+//     });
 
-    $('.flip-card').on('click', () => {
-        $(this).toggleClass('flipped');
-    });
+//     $('.flip-card').on('click', () => {
+//         $(this).toggleClass('flipped');
+//     });
 
-    const thisyear = new Date().getFullYear();
-    ["year", "year0", "year1"].forEach(id => {
-        const element = document.getElementById(id);
-        if (element) {
-            element.innerHTML = thisyear;
-        }
-    });
+// });
+
+const thisyear = new Date().getFullYear();
+["year", "year0", "year1"].forEach(id => {
+    const element = document.getElementById(id);
+    if (element) {
+        element.innerHTML = thisyear;
+    }
 });
 
 function copyToClipboard(element) {
--- js/main404.js
+++ js/main404.js
@@ -1,7 +1,5 @@
-$(document).ready(() => {
-    const thisyear = new Date().getFullYear();
-        const element = document.getElementById("year");
-        if (element) {
-            element.innerHTML = thisyear;
-        }
-});
+const thisyear = new Date().getFullYear();
+const element = document.getElementById("year");
+if (element) {
+    element.innerHTML = thisyear;
+}
